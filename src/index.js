ada8085bf401855ed9f06976677757266bbd2df3
Debasish Das Biswas <ddebu168@gmail.com>
2024-07-10 13:52:12 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import connectDB from "./db/index.js";
-import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
+require("dotenv").config({ path: "./.env" });
 import { app } from "./app.js";
 const port = 5000;
 connectDB()
